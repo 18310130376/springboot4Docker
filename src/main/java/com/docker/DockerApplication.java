387929c1d8c9982ad6e1be279a8b1f640d0e9cdc
475402366@qq.com <475402366@qq.com>
2018-07-15 23:25:31 +0800

    #

--- src/main/java/com/docker/DockerApplication.java
+++ src/main/java/com/docker/DockerApplication.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
-@SpringBootApplication
+@SpringBootApplication 
 @RestController
 public class DockerApplication {
     
