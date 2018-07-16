9b1eef5251cfba7b3b6df567528c07327b1314b7
Abbott Au <abbott.wu@ixdigit.com>
2018-07-16 19:08:50 +0800

    #

--- src/main/java/com/docker/DockerApplication.java
+++ src/main/java/com/docker/DockerApplication.java
@@ -1,14 +1,9 @@
 package com.docker;
 
-import java.util.List;
-import java.util.Map;
-
 import javax.servlet.http.HttpServletRequest;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,8 +13,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class DockerApplication {
     
-   @Autowired
-   private JdbcTemplate jdbcTemplate;
+//   @Autowired
+//   private JdbcTemplate jdbcTemplate;
     
     @RequestMapping("/getUserInfo")
     public String getUserInfo(HttpServletRequest qequest) {
@@ -28,12 +23,13 @@ public class DockerApplication {
     	if(StringUtils.isEmpty(id)) {
     		return "id must not be null";
     	}
+		return "hello";
     	
-    	List<Map<String, Object>> queryForList = jdbcTemplate.queryForList("select * from docker_user where id="+id);
-    	if(queryForList == null || queryForList.size() == 0) {
-    		return "result is null";
-    	}
-		return (String) queryForList.get(0).get("username");
+//    	List<Map<String, Object>> queryForList = jdbcTemplate.queryForList("select * from docker_user where id="+id);
+//    	if(queryForList == null || queryForList.size() == 0) {
+//    		return "result is null";
+//    	}
+//		return (String) queryForList.get(0).get("username");
     }
 
     public static void main(String[] args) {
