8a36286dac086267377bdb98ef7b965eccf6db7b
475402366@qq.com <475402366@qq.com>
2018-07-19 21:49:16 +0800

    #

--- src/main/java/com/docker/service/UserProviderService.java
+++ src/main/java/com/docker/service/UserProviderService.java
@@ -4,14 +4,11 @@ import java.util.List;
 
 import com.alibaba.dubbo.config.annotation.Service;
 
-
+@SuppressWarnings("all")
 @Service
 public class UserProviderService implements IUserProviderService{
-
 	
-	@SuppressWarnings("rawtypes")
 	public List getAllUser(String username) {
-		
 		return null;
 	}
 }
