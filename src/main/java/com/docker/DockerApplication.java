f1c4ceb92f35319a439f5d227be3574cd3f3d97f
475402366@qq.com <475402366@qq.com>
2018-08-05 20:12:02 +0800

    #

--- src/main/java/com/docker/DockerApplication.java
+++ src/main/java/com/docker/DockerApplication.java
@@ -1,28 +1,22 @@
 package com.docker;
 
-import java.util.concurrent.CountDownLatch;
-
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.context.annotation.Bean;
 
-@SpringBootApplication 
+@SpringBootApplication
 public class DockerApplication {
 
-	private static final Log logger = LogFactory.getLog(DockerApplication.class);
-	
-    @Bean
-	public CountDownLatch closeLatch() {
-		return new CountDownLatch(1);
+	private static final Log logger = LogFactory
+			.getLog(DockerApplication.class);
+
+	public static void main(String[] args) throws InterruptedException {
+		
+		 //PropertyConfigurator.configure(System.getProperty("user.dir") + "/conf/log4j.properties"); 
+		System.out.println(System.getProperty("user.dir") + "/conf/log4j.properties");
+		new SpringApplicationBuilder().sources(DockerApplication.class).run(
+				args);
+		logger.info("====== Project started successfull ======");
 	}
-    
-    public static void main(String[] args) throws InterruptedException{
-    	
-			new SpringApplicationBuilder().sources(DockerApplication.class).web(false).run(args);
-			logger.info("====== Project started successfull ======");
-		   //CountDownLatch closeLatch = ctx.getBean(CountDownLatch.class);
-		   //closeLatch.await();
-   }
 }
--- src/main/java/com/docker/service/UserProviderService.java
+++ src/main/java/com/docker/service/UserProviderService.java
@@ -2,10 +2,6 @@ package com.docker.service;
 
 import java.util.List;
 
-import com.alibaba.dubbo.config.annotation.Service;
-
-@SuppressWarnings("all")
-@Service
 public class UserProviderService implements IUserProviderService{
 	
 	public List getAllUser(String username) {
